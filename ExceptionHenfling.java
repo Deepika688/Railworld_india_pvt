6d41968ab595e9bbb13720070eeee1c86d01f182
Deepika <solankideepika7919@gmail.com>
2024-04-23 22:54:25 +0530

    ExceptionHendling Commit

---
+++ ExceptionHenfling.java
@@ -0,0 +1,12 @@
+package Railworld_India_PVT_Task;
+
+public class ExceptionHenfling {
+    public static void main(String[] args) {
+        try{
+            //code that may raise exception
+            int data=100/0;
+        }catch(ArithmeticException e){System.out.println(e);}
+        //rest code of the program
+        System.out.println("rest of the code...");
+    }
+}
