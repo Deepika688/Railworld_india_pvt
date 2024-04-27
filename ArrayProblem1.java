7aef0191d115704063e6701598dc6b22724a73a9
Deepika <solankideepika7919@gmail.com>
2024-04-27 17:06:14 +0530

    Array codes Commit

---
+++ ArrayProblem1.java
@@ -0,0 +1,36 @@
+package Railworld_India_PVT_Task;
+import java.io.*;
+
+public class ArrayProblem1 {
+
+
+
+    static int getMin(int arr[], int i, int n)
+    {
+        // If there is single element, return it.
+        // Else return minimum of first element and
+        // minimum of remaining array.
+        return (n == 1) ? arr[i] : Math.min(arr[i],
+                getMin(arr,i + 1 , n - 1));
+    }
+
+    static int getMax(int arr[], int i, int n)
+    {
+        // If there is single element, return it.
+        // Else return maximum of first element and
+        // maximum of remaining array.
+        return (n == 1) ? arr[i] : Math.max(arr[i],
+                getMax(arr ,i + 1, n - 1));
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        int arr[] = { 12, 1234, 45, 67, 1 };
+        int n = arr.length;
+        System.out.print("Minimum element of array: " +
+                getMin(arr, 0, n) + "\n");
+        System.out.println("Maximum element of array: " +
+                getMax(arr, 0, n));
+    }
+}
